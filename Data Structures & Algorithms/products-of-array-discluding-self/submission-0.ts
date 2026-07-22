e0a7966ace49a98df65176786444624afcd8a1fe
Furkan <furkan.builds@gmail.com>
2026-07-22 23:32:29 +0100

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const answer = new Array(nums.length);
+
+        let leftProduct = 1;
+        let rightProduct = 1;
+
+        for (let i = 0; i < nums.length; i++) {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        for (let i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+}
